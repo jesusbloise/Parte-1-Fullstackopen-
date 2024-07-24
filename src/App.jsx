e133707aa98a41084da1d437687021cbc4990b6e
jesusbloise <jesusbloise@gmail.com>
2024-07-23 20:29:22 -0400

    ejercico 1.6 listo hasta aqui

--- src/App.jsx
+++ src/App.jsx
@@ -1,137 +1,38 @@
+//No definir componentes dentro de los componentes
+import { useState } from 'react'
 
-// aqui le metimos javaScript y lo dejamos en la consola
-import React from 'react';
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
+  // const [value, setValue] = useState(10)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  // Calcular el total de ejercicios
-  const totalExercises = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
-  console.log('Total de ejercicios:', totalExercises);
-
-  return null; // No renderizamos nada en la interfaz
-};
-
-export default App;
-// hasta aqui
-
-//parte 
-//Titulo con su nombre en forma de props
-// const course = 'Half Stack application development'
-// const Header = (props) => {
-//   console.log(props)
-//   return (
-//     <>
-//       <h1>Titulo del curso {course}</h1>
-//     </>
-//   )
-// }
-//parte y ejercicios de un forma 
-// const Content = (props) => {
-//   console.log(props)
-//   return (
-//     <>
-//      <p>
-//         {props.part1} {props.exercises1}
-//         {props.part2} {props.exercises2}
-//         {props.part3} {props.exercises3}
-//       </p>
-//     </>
-//   )
-// }
-//   const part1 = 'Fundamentals of React'
-//   const exercises1 = 10
-//   const part2 = 'Using props to pass data'
-//   const exercises2 = 7
-//   const part3 = 'State of a component'
-//   const exercises3 = 14
-//   const Total = (props) => {
-//   console.log(props)
-//   return (
-//     <>
-//      <p>
-//       Number of exercises {exercises1+exercises2+exercises3}
-//       </p>
-//     </>
-//   )
-// }
-// const App = () => {
-//     // parte y ejercicio de otra forma
-//    const Content = [
-//       {part1:'Fundamentals of React', exercises1:10},
-//       {part2:'Using props to pass data', exercises2:7},
-//       {part3:'State of a component', exercises3:14}
-//    ]
-
-//   return (
-//     <div>
-//       <Header course={course} />
-//       {/* <Content part1={part1} exercises1={exercises1}/>
-//       <Content part2={part2} exercises2 ={exercises2}/>
-//       <Content part3={part3} exercises3 ={exercises3}/> */}
-//       <p>{Content[0].part1} {Content[0].exercises1}</p>
-//       <p>{Content[1].part2} {Content[1].exercises2}</p>
-//       <p>{Content[2].part3} {Content[2].exercises3}</p>
-//       <Total Total={Total}/>
-
-//     </div>
-//   )
-// }
-// export default App
-
-
-
-// esto para abajo son ejemplos 
-// const Hello = (props) => {
-//   console.log (props)
-//   return (
-//     <div>
-//       <p>Hello {props.name}, you are {props.age} years old
-//       </p>
-//     </div>
-//   )
-// }
-// const Footer = () => {
-//   return (
-//     <div>
-//       greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
-//     </div>
-//   )
-// }
-// const App = () => {
-//   const amigos = ['Peter', 'Maya']
-//   const friends = [
-//     { name: 'Peter', age: 4 },
-//     { name: 'Maya', age: 10 },
-//   ]
-//   const name = 'Peter'
-//   const age = 10
-//   return (
-//     <>
-//       <h1>Greetings</h1>
-//       <Hello name='Maya' age={20 + 10}/>
-//       <Hello name={name} age={age} />
-//       <p>{amigos + ' '}</p>
-//       <p>{friends[0].name} {friends[0].age}</p>
-//       <p>{friends[1].name} {friends[1].age}</p>
-//       <Footer/>
-//     </>
-//   )
-// }
-// export default App
+  // const setToValue = (newValue) => {
+  //   console.log('value now', newValue) //imprime el nuevo valor en la consola
+  //   setValue(newValue)
+  // }
+  const setToGood = (newValue) => {
+    console.log('value now', newValue)
+    setGood(newValue)
+  }
+ 
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={()=> setToGood(good + 1)} text='good' />
+      <Button handleClick={()=>setNeutral(neutral + 1)} text='neutral'/>
+      <Button handleClick={()=>setBad(bad + 1)} text='bad'/>
+      <h2>statisties</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+export default App
+//hasta aqui
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,36 @@
+
+
+//componente conestado desde aqui
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+//hasta aqui
+
+//para re renderizado de aqui
+// import ReactDOM from 'react-dom/client'
+
+// import App from './App'
+
+// let counter = 1
+
+// const refresh = () => {
+//   ReactDOM.createRoot(document.getElementById('root')).render(
+//     <App counter={counter} />
+//   )
+// }
+// setInterval(() => {
+//   refresh()
+//   counter += 1
+// }, 1000)
+//hasta aqui
+
+
+//normal para otras funciones de aqui 
+// import ReactDOM from 'react-dom/client'
+
+// import App from './App'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+//hasta aqui
