eb8d0bf9b4dc2ddbde14a2e9bdbf1c003eeb03e9
jesusbloise <jesusbloise@gmail.com>
2024-07-25 20:11:04 -0400

    ejercicio 2.8 listo agregando el numero

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import { useState } from 'react';
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
-
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
   const addPerson = (event) => {
     event.preventDefault();
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
     setPersons(persons.concat(newPerson));
     setNewName('');
+    setNewNumber('');
   };
 
   return (
@@ -27,13 +31,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
