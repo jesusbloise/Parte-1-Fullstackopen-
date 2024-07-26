594b6cba176b96478387797122349e8ce5c082f2
jesusbloise <jesusbloise@gmail.com>
2024-07-25 20:34:31 -0400

    ejercicio 2.9 creando el filtro que funciona con mayusculas y minusculas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '123-456-7890' },
+    { name: 'Ada Lovelace', number: '234-567-8901' },
+    { name: 'Dan Abramov', number: '345-678-9012' },
+    { name: 'Mary Poppendieck', number: '456-789-0123' }
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -11,9 +17,12 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
   const addPerson = (event) => {
     event.preventDefault();
-    if (persons.some(person => person.name === newName)) {
+    if (persons.some(person => person.name.toLowerCase() === newName.toLocaleLowerCase())) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
@@ -22,16 +31,22 @@ const App = () => {
     setNewName('');
     setNewNumber('');
   };
-
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search: <input value={searchTerm} onChange={handleSearchChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -39,12 +54,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => (
+        {filteredPersons.map(person => (
           <li key={person.name}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
   );
 };
-
 export default App;
